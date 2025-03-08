694a2772e56170da66dcc5cef64e0f74d9709e8d
ducampcarlos <ducampcarlos@gmail.com>
2025-03-08 12:54:06 -0300

    Adjusted player movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,7 +3,9 @@ using UnityEngine.InputSystem;
 
 public class PlayerController : MonoBehaviour
 {
+    [Header("Movement")]
     public InputAction MoveAction;
+    [SerializeField] private float speed = 5f;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -16,7 +18,7 @@ public class PlayerController : MonoBehaviour
     {
         Vector2 move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * 0.1f;
+        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
         transform.position = position;
     }
 }
