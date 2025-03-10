dd42dba9ba3f5d9cd5bdb1e8fa29644a29305e23
ducampcarlos <ducampcarlos@gmail.com>
2025-03-10 10:59:00 -0300

    Changed player movement from transform movement to rigidbody movement.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -6,19 +6,26 @@ public class PlayerController : MonoBehaviour
     [Header("Movement")]
     public InputAction MoveAction;
     [SerializeField] private float speed = 5f;
+    Rigidbody2D rb;
+    Vector2 move;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector2 move = MoveAction.ReadValue<Vector2>();
+        move = MoveAction.ReadValue<Vector2>();
         Debug.Log(move);
-        Vector2 position = (Vector2)transform.position + move * speed * Time.deltaTime;
-        transform.position = position;
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = (Vector2)rb.position + move * speed * Time.deltaTime;
+        rb.MovePosition(position);
     }
 }
