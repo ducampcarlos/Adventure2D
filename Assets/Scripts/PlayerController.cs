64f74f9d4a6ab6e280dc8ab8d3a1a6d781dc4b52
ducampcarlos <ducampcarlos@gmail.com>
2025-03-10 11:22:59 -0300

    Erased unnecesary folder not used. Added Health collectible.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,23 +9,34 @@ public class PlayerController : MonoBehaviour
     Rigidbody2D rb;
     Vector2 move;
 
+    [Space]
+    [Header("Lives")]
+    [SerializeField] int maxHealth = 5;
+    int currentHealth;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         MoveAction.Enable();
         rb = GetComponent<Rigidbody2D>();
+        currentHealth = maxHealth;
     }
 
     // Update is called once per frame
     void Update()
     {
         move = MoveAction.ReadValue<Vector2>();
-        Debug.Log(move);
+        //Debug.Log(move);
     }
 
     void FixedUpdate()
     {
         Vector2 position = (Vector2)rb.position + move * speed * Time.deltaTime;
         rb.MovePosition(position);
+    }
+
+    public void ChangeHealth(int amount)
+    {
+        currentHealth = Mathf.Clamp(currentHealth + amount, 0, maxHealth);
     }
 }
