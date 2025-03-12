a7d577ea9c2d190c76b41305d65fe225e31335c9
ducampcarlos <ducampcarlos@gmail.com>
2025-03-12 09:32:32 -0300

    Added basic movement to enemy.

--- Assets/Scripts/EnemyController.cs
+++ Assets/Scripts/EnemyController.cs
@@ -2,15 +2,46 @@ using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [Header("Parameters")]
+    [SerializeField] float speed;
+    Rigidbody2D rigidbody2d;
+    [SerializeField] float changeTime = 3.0f;
+    float timer;
+    int direction = 1;
+
+    [Header("Direction")]
+    [SerializeField] bool vertical;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        rigidbody2d = GetComponent<Rigidbody2D>();
+        timer = changeTime;
     }
 
-    // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        
+        timer -= Time.deltaTime;
+        if (timer < 0)
+        {
+            direction = -direction;
+            timer = changeTime;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 position = rigidbody2d.position;
+
+        if (vertical)
+        {
+            position.y = position.y + speed * direction * Time.deltaTime;
+        }
+        else
+        {
+            position.x = position.x + speed * direction * Time.deltaTime;
+        }
+
+        rigidbody2d.MovePosition(position);
     }
 }
