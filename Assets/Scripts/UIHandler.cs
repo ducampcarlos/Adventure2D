c46f9ee92aa5f6adb9bacaa7357e64c711d05619
ducampcarlos <ducampcarlos@gmail.com>
2025-03-13 10:06:09 -0300

    Added logic to show dialogue box.

--- Assets/Scripts/UIHandler.cs
+++ Assets/Scripts/UIHandler.cs
@@ -7,6 +7,10 @@ public class UIHandler : MonoBehaviour
 
     private VisualElement m_Healthbar;
 
+    [SerializeField] float displayTime = 4.0f;
+    private VisualElement m_NonPlayerDialogue;
+    private float m_TimerDisplay;
+
     private void Awake()
     {
         instance = this;
@@ -18,11 +22,35 @@ public class UIHandler : MonoBehaviour
         UIDocument uiDocument = GetComponent<UIDocument>();
         m_Healthbar = uiDocument.rootVisualElement.Q<VisualElement>("HealthBar");
         SetHealthValue(1.0f);
+
+        m_NonPlayerDialogue = uiDocument.rootVisualElement.Q<VisualElement>("NPCDialogue");
+        m_NonPlayerDialogue.style.display = DisplayStyle.None;
+        m_TimerDisplay = -1.0f;
+    }
+
+    private void Update()
+    {
+        if (m_TimerDisplay > 0)
+        {
+            m_TimerDisplay -= Time.deltaTime;
+            if (m_TimerDisplay < 0)
+            {
+                m_NonPlayerDialogue.style.display = DisplayStyle.None;
+            }
+        }
+
+
     }
 
     public void SetHealthValue(float percentage)
     {
         m_Healthbar.style.width = Length.Percent(100 * percentage);
+    }
+
+    public void DisplayDialogue()
+    {
+        m_NonPlayerDialogue.style.display = DisplayStyle.Flex;
+        m_TimerDisplay = displayTime;
     }
 
 }
